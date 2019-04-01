a19e451daad9ee09c320370e647a23a6129ffc7d
diCharles <43318661+diCharles@users.noreply.github.com>
2019-04-01 10:19:16 -0600

    rgb_adc imprime en pantalla

--- rgb_adc.h
+++ rgb_adc.h
@@ -8,9 +8,14 @@
 #ifndef RGB_ADC_H_
 #define RGB_ADC_H_
 
+#define VREF_ADC 3.3F
+#define RESOLUTION_ADC 255U
 
 #include "global.h"
 /*when called the application rgb_adc will be executed*/
 void exe_rgb_adc(void);
 
+/* prints voltage in format decimal XX.YY*/
+void print_ADC_read(uint8_t convertion_adc);
+void ADC_convertion_to_rgb(uint8_t convertion_adc);
 #endif /* RGB_ADC_H_ */
